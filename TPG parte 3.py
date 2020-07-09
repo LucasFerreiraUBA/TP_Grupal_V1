3744f5385261e69abb198b273dee08939d8f966c
LucasDiana <65971234+LucasDiana@users.noreply.github.com>
2020-07-09 20:40:24 -0300

    funcion de proceso de csv (p1)
    Por ahora solo genera una lista de funciones.
    
    Falta generar una lista de llamadas a esta funcion

--- TPG parte 3.py
+++ TPG parte 3.py
@@ -17,8 +17,7 @@ def contador_de_invocaciones(listaOG, listaLL):
         dixTOT = {'funcion1()': [0, 2, 1, 0],
                   'funcion2()': [1, 0, 1, 1],
                   'funcion3()': [1, 1, 0, 0],
-                  'funcion4()': [0, 1, 0, 0]}    (es correcto!)
-    """
+                  'funcion4()': [0, 1, 0, 0]}    (es correcto!) """
     
 ##  SOLO LLAMAR AL CONTADOR DE INVOCACIONES SI EL DIX NO EXISTE!!  
 ##  listaLL = ["lista con todas las llamadas a otras funciones"]
@@ -44,13 +43,50 @@ def contador_de_invocaciones(listaOG, listaLL):
                 """... con cada llamada [si la funcion(llamada)
                         no se la llama, queda el valor en cero]"""
                 if fc2 == call:                    
-                    sublis[contPosS] += 1                    
+                    sublis[contPosS] += 1
+            del call
             contPosS += 1
         """cuando termino de chequear las llamadas de la primer funcion,
             la sumo al diccionario y paso a la siguiente"""
+        del fc2, contPosS
         contPosLL += 1        
         dixTOT[func] = sublis
     """Una vez calculadas todas las llamadas del programa,
         devuelvo el diccionario"""
 ##  SOLO LLAMAR AL CONTADOR DE INVOCACIONES SI EL DIX NO EXISTE!!
+    del func, sublis, contPosLL
     return dixTOT
+
+
+def procesador_de_csv():
+    """
+            [Autor: Lucas M. Diana]
+            Genera una lista con todas las funciones que existen
+            dentro de fuente_unico.csv
+
+            para testear su funcionamiento,
+            use este archivo guardado como .csv
+
+            devolvio:
+            
+            listaOG = ['contador_de_invocaciones',
+            'generador_de_lista_de_funciones']       [es correcto! (x ahora)]
+    """
+
+    fuente_unico =  open('fuente_unico.csv','r')
+    listaFuncP = []
+    listaOG = []
+    for i in fuente_unico:
+        if i.startswith("def "):
+            listaFuncP.append(i.replace('def ',''))
+    del i
+    for j in listaFuncP:
+        paren = j.find('(')
+        funcion = j[:paren]
+        listaOG.append(funcion)
+        del paren, funcion, j
+    del listaFuncP
+    """Hasta aca genera la lista de funciones del programa
+        Continuo con la lista de llamadas"""
+    listaLL = []
+    return listaOG, listaLL

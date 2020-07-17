8a427c68e585d207722f16dcb59f02fae7070bda
LucasDiana <65971234+LucasDiana@users.noreply.github.com>
2020-07-17 17:07:23 -0300

    ARC 95% completado
    'printer()' es efectivamente la 'main' del modulo - usar 'printer' hace todo
    
    Prolijidad(?????)

--- TPG parte 3.py
+++ TPG parte 3.py
@@ -1,31 +1,32 @@
-import os
+import os, time
 
-def contador_de_invocaciones(listaOG): 
+def contador_de_invocaciones(listaOG, listaLL): 
     """
     [Autor: Lucas M. Diana]
     Genera un diccionario con la cantidad de referencias
     que realiza cada funcion.
-    
+
     """
-      
+
     dixTOT = {}
-    contPosLL = 0
-    for func in listaOG:
-        dixTOT[func] = []     
-        contPosS = 0
+    posLL = 0
+    for funcion in listaOG:
+        dixTOT[funcion] = []     
+        posSub = 0
         sublis = []
-        for fc2 in listaOG:
+        for funcion2 in listaOG:
             sublis.append(0)      
-            sublis[contPosS] = 0            
-            for call in listaLL[contPosLL]:
-                if fc2 == call:                    
-                    sublis[contPosS] += 1
-            contPosS += 1
-        contPosLL += 1        
-        dixTOT[func] = sublis
+            sublis[posSub] = 0            
+            for call in listaLL[posLL]:
+                if funcion2 == call:                    
+                    sublis[posSub] += 1
+            posSub += 1
+        posLL += 1        
+        dixTOT[funcion] = sublis
+    del posLL, funcion, posSub, sublis, funcion2, call
     return dixTOT
 
-def contador_de_llamadas(listaOG, listaLL): 
+def contador_de_llamadas(listaOG, dixTOT): 
     """
     [Autor: Lucas M. Diana]
     Genera un diccionario que marca donde es referenciada
@@ -44,6 +45,7 @@ def contador_de_llamadas(listaOG, listaLL):
                 listaRec.append('O')
         dixRec[recive] = listaRec
         pos += 1
+    del pos, recive, listaRec, envia
     return dixRec
 
 
@@ -54,18 +56,19 @@ def generador_de_lista_de_funciones():
     dentro de fuente_unico.csv
     
     """
-
+    
     fuente_unico =  open('fuente_unico.csv','r')
-    listaFuncP = []
+    lista_de_funciones_parcial = []
     listaOG = []
-    for i in fuente_unico:
-        if i.startswith("def "):
-            listaFuncP.append(i.replace('def ',''))
-    for j in listaFuncP:
-        paren = j.find('(')
-        funcion = j[:paren]
+    for valor in fuente_unico:
+        if valor.startswith("def "):
+            lista_de_funciones_parcial.append(valor.replace('def ',''))
+    for proto_funcion in lista_de_funciones_parcial:
+        paren = proto_funcion.find('(')
+        funcion = proto_funcion[:paren]
         listaOG.append(funcion)
     fuente_unico.close()
+    del lista_de_funciones_parcial, valor, proto_funcion, paren, funcion
     return listaOG
 
 def generador_de_listas_de_llamadas():
@@ -75,46 +78,48 @@ def generador_de_listas_de_llamadas():
     que existen dentro. Estas se incorporan a una lista de listas.
     
     """
+    
     fuente_unico =  open('fuente_unico.csv','r')
-    listaFParcial = []
+    lista_de_funciones_parcial = []
     listaLL = []
-    for a in fuente_unico:
-        if a.startswith("def ") and listaFParcial != []:
-            sublistaLL = procesar_listaFP(listaFParcial)
+    for valor in fuente_unico:
+        if valor.startswith("def ") and lista_de_funciones_parcial != []:
+            sublistaLL = procesar_listaFP(lista_de_funciones_parcial)
             listaLL.append(sublistaLL)
-            listaFParcial = []
-            listaFParcial.append(a)
+            lista_de_funciones_parcial = []
+            lista_de_funciones_parcial.append(valor)
+        elif valor is None:
+            sublistaLL = procesar_listaFP(lista_de_funciones_parcial)
+            listaLL.append(sublistaLL)
         else:
-            listaFParcial.append(a)
-    
-##  Para la ultima, podria resolverse con eof
-    
-    sublistaLL = procesar_listaFP(listaFParcial)
-    listaLL.append(sublistaLL)
-    
+            lista_de_funciones_parcial.append(valor)
+    fuente_unico.close()
+    del lista_de_funciones_parcial, valor, sublistaLL
     return listaLL
 
-def procesar_listaFP(listaFParcial):
+def procesar_listaFP(lfp):
     """
     [Autor: Lucas M. Diana]
     Filtra la lista con cada linea de codigo de una funcion
     y devuelve una lista que contiene solo las llamadas de la funcion
     
     """
+    
     sublistaLL = []
-    for b in listaFParcial:
+    for funcionLocal in lfp:
         llamada = None
-        if b.endswith(')'):
-            paren = b.find('(')
-            if '= ' in b:
-                igual = b.find('= ') + 2
-            elif '=' in b:
-                igual = b.find('=') + 1
+        if funcionLocal.endswith(')'):
+            paren = funcionLocal.find('(')
+            if '= ' in funcionLocal:
+                igual = funcionLocal.find('= ') + 2
+            elif '=' in funcionLocal:
+                igual = funcionLocal.find('=') + 1
             else:
                 igual = 0
-            llamada = b[igual:paren]
+            llamada = funcionLocal[igual:paren]
             if llamada in listaOG:
                 sublistaLL.append(llamada)
+    del paren, igual, funcion, llamada, funcionLocal
     return sublistaLL
 
 def txt_maker(listaOG, dixTOT, dixRec):
@@ -131,7 +136,9 @@ def txt_maker(listaOG, dixTOT, dixRec):
     for funcion in listaOG:
         if len(funcion) > longitud:
             longitud = len(funcion)
+    longitud += 1
     linea.append('|\t{0:{1}} ').format('Funcion/es', longitud)
+    del longitud
     contador = 0
     for funcion in listaOG:
         linea.append('| {} ').format(contador)
@@ -143,9 +150,10 @@ def txt_maker(listaOG, dixTOT, dixRec):
 ##  Genera el separador de renglones"""
     
     separador = ''
-    for i in len(linea):
+    for char in len(linea):
         separador.append('-')
     separador.append('\n')
+    del char
     
 ##  Genera y suma el resto de las lineas
 ##  y los separadores de renglones que correspondan.
@@ -165,48 +173,42 @@ def txt_maker(listaOG, dixTOT, dixRec):
             else:
                 linea.append('|   ')
             contador += 1
+        linea.append('|\n')
         analizador.write(linea)
     analizador.write(separador)
     analizador.close()
+    del funcion, funcion2, linea, contador
 
 def printer():
     """
     [Autor: Lucas M. Diana]
-    Imprime los contenidos del archivo "analizador.txt" en la pantalla.
+    Crea e imprime los contenidos del archivo "analizador.txt" en la pantalla.
     
     """
+    
     if os.isfile(os.getcwd()\\analizador.txt):
         print('analizador.txt existe, leyendo...')
+        time.sleep(2)
         os.system('cls')
         analizador = open(analizador.txt, 'r')
     else:
         print('analizador.txt no existe, creando...')
-        txt_maker()
+        listaOG = generador_de_lista_de_funciones()
+        listaLL = generador_de_listas_de_llamadas()
+        dixTOT = contador_de_invocaciones(listaOG, listaLL)
+        dixRec = contador_de_llamadas(listaOG, dixTOT)
+        txt_maker(listaOG, dixTOT, dixRec)
+        del listaOG, listaLL, dixRec, dixTOT
+        time.sleep(2)
         print('analizador.txt creado, leyendo...')
+        time.sleep(2)
         os.system('cls')
         analizador = open(analizador.txt, 'r')
-    for i in analizador:
-        linea = ''
-        linea.append(i)
-        if linea.endswith('\n'):
-            print(linea)
+    print(analizador.read())
     analizador.close()
 
-"""
-Ejecucion del modulo
-
-listaOG = generador_de_lista_de_funciones()
-listaLL = generador_de_listas_de_llamadas() 
-dixTOT = contador_de_invocaciones(listaOG)
-dixRec = contador_de_llamadas(listaOG, listaLL)
-txt_maker(listaOG, dixTOT, dixRec)
-printer()
-
-
-Falta un metodo para volver al programa principal
-Chequear si es mejor dejar que las funciones se invoquen entre si
-O ejecutar el modulo en su totalidad.
-Renombrar todas las variables para hacer mas legible el codigo.
-Ver si es posible reducir la cantidad de 'for's y/o crear una funcion
-que pueda resolver los casos de 'for' anidados
-"""
+## Falta un metodo para volver al programa principal (de ser necesario)
+## Solo usar printer() - hace todo lo demas
+## Renombrar variables confusas (?) para hacer mas legible el codigo.
+## Ver si es posible reducir la cantidad de 'for's (cada uno tiene su uso) T_T
+## time.sleep(n) no hace nada durante n segundos

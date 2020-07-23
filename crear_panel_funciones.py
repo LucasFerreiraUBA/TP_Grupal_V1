feb01a885a4c8023b5ae93324b1fe423a5afc404
javier-gh <67992620+javier-gh@users.noreply.github.com>
2020-07-23 10:51:56 -0300

    Add files via upload

---
+++ crear_panel_funciones.py
@@ -0,0 +1,24 @@
+def mostrar_tabla_funciones(arch_datos_funciones):
+	"""[Autor: Javier Acho]
+	   [Ayuda: Recibe un archivo con extensión csv y muestra por pantalla una
+	    tabla con información de cada funcion que se encuentra dentro del
+	    archivo.
+	    El archivo que recibe debe estar abierto en modo lectura.]
+	"""
+
+	for linea in arch_datos_funciones:
+		c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13 = \
+		linea.rstrip("\n").split(",")
+
+		cadena = "{0:<30s} {1:<11s} {2:<7s} {3:<13s}".format(c1, c2, c3, c4) +\
+		         "{0:<8s} {1:<8s} {2:<4s} {3:<6s}".format(c5, c6, c7, c8) +\
+		         "{0:<6s}{1:<5s} {2:<7s} {3:<6}".format(c9, c10, c11, c12) +\
+		         " {0:<20s}".format(c13)
+
+		print(cadena)
+
+#------------------------------------------------------------------------------
+
+arch_datos = open("panel_general.csv", "r")
+mostrar_tabla_funciones(arch_datos)
+arch_datos.close()

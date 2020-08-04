51d135db3f5c33c5cf3b10e4f86c138576d0fd04
LucasDiana <65971234+LucasDiana@users.noreply.github.com>
2020-08-04 19:39:50 -0300

    Final??

--- crear_panel_funciones.py
+++ crear_panel_funciones.py
@@ -10,7 +10,7 @@ def mostrar_tabla_funciones(arch_datos_funciones):
 		c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13 = \
 		linea.rstrip("\n").split(",")
 
-		cadena = "{0:<30s} {1:<11s} {2:<7s} {3:<13s}".format(c1, c2, c3, c4) +\
+		cadena = "{0:<50s} {1:<11s} {2:<7s} {3:<13s}".format(c1, c2, c3, c4) +\
 		         "{0:<8s} {1:<8s} {2:<4s} {3:<6s}".format(c5, c6, c7, c8) +\
 		         "{0:<6s}{1:<5s} {2:<7s} {3:<6}".format(c9, c10, c11, c12) +\
 		         " {0:<20s}".format(c13)
